cd8730e1393398c73165987d2dee0153b7ce2575
Somnath-2024-40 <163083739+Somnath-2024-40@users.noreply.github.com>
2025-08-05 19:54:25 +0530

    information_gathering.py

--- information_gathering.py
+++ information_gathering.py
@@ -1,6 +1,6 @@
 from importlib.metadata import requires
 from sys import argv, exit
-
+from colorama import init,Fore
 import requests
 from whois import whois
 from dns import resolver
@@ -9,6 +9,13 @@ import requests
 from argparse import ArgumentParser
 import socket
 # here I import my all libraries.
+
+red=Fore.RED
+blue=Fore.BLUE
+yellow=Fore.YELLOW
+cyan=Fore.CYAN
+reset=Fore.RESET
+
 
 argument_parser = ArgumentParser(
     description="This tool is used for information gathering",
@@ -30,77 +37,77 @@ domain=args.domain
 # then have made an object named "argument_parser" and initialized  keyword arguments (or named parameters)
 
 # whois module
-if not (args.dns or args.geo or args.all):
+if not (args.dns or args.geo) or args.all:
+
     try:
-        print("[+] whois getting info....")
+        print(f"{red}[+] whois getting info....{reset}")
         py=whois(domain)
-        print("[+] whois information found. ")
-        print("name: {}".format(py.name))
-        print("registrar:{}".format(py.registrar))
-        print("Creation date: {}".format(py.creation_date))
-        print("Expiration date: {}".format(py.expiration_date))
-        print("Registrant: {}".format(py.registrant))
-        print("Registrant country: {}".format(py.registrant_country))
+        print(f"{red}[+] whois information found.{reset} ")
+        print(f"{blue}name:{reset} {yellow}{py.name}{reset}")
+        print(f"{blue}registrar:{reset}{yellow}{py.registrar}{reset}")
+        print(f"{blue}Creation date:{reset} {yellow}{py.creation_date}{reset}")
+        print(f"{blue}Expiration date:{reset} {yellow}{py.expiration_date}{reset}")
+        print(f"{blue}Registrant:{reset}{yellow} {py.registrant}{reset}")
+        print(f"{blue}Registrant country:{reset} {yellow} {py.registrant_country}{reset}")
     # whois module used for domain information
     except:
         pass
 print()
 print()
 
-print("----------------------------------------------------------------------------")
+print(f"{cyan}----------------------------------------------------------------------------{reset}")
 # dns module
 if args.dns or args.all:
-    print("[+] getting DNS information....")
+    print(f"{red}[+] getting DNS information....{reset}")
     try:
         for a in resolver.resolve(domain,'A'):
-            print("[+] A record: {}".format(a.to_text()))
+            print(f"{blue}[+] A record:{reset} {yellow}{a.to_text()}{reset}")
 
     except:
         pass
     try:
         for MX in resolver.resolve(domain, 'MX'):
-            print("[+] MX record: {}".format(MX.to_text()))
+            print(f"{blue}[+] MX record:{reset} {yellow}{MX.to_text()}{reset}")
 
     except:
         pass
     try:
         for NS in resolver.resolve(domain, 'NS'):
-            print("[+] NS record: {}".format(NS.to_text()))
+            print(f"{blue}[+] NS record:{reset} {yellow}{NS.to_text()}{reset}")
 
     except:
         pass
     try:
         for TXT in resolver.resolve(domain, 'TXT'):
-            print("[+] TXT record: {}".format(TXT.to_text()))
+            print(f"{blue}[+] TXT record:{reset} {yellow}{TXT.to_text()}{reset}")
 
     except:
         pass
     try:
         for AAAA in resolver.resolve(domain, 'AAAA'):
-            print("[+] AAAA record: {}".format(AAAA.to_text()))
+            print(f"{blue}[+] AAAA record:{reset}{yellow} {AAAA.to_text()}{reset}")
 
     except:
         pass
 print()
 print()
-print("----------------------------------------------------------------------------")
+print(f"{cyan}----------------------------------------------------------------------------{reset}")
 # module geolocation
 if args.geo or args.all:
-    print("[+++++] getting geo location information....[+++++]")
+    print(f"{red}[+++++] getting geo location information....[+++++]{reset}")
     try:
         response = requests.request('GET', "https://geolocation-db.com/json/" + socket.gethostbyname(domain)).json()
-        print("[+] found geolocation information  .")
-        print("[+] COUNTRY NAME: {}".format(response['country_name']))
-        print("[+] LATITUDE: {}".format(response['latitude']))
-        print("[+] LONGITUDE: {}".format(response['longitude']))
-        print("[+] CITY: {}".format(response['city']))
-        print("[+] COUNTRY CODE: {}".format(response['country_code']))
-        print("[+] STATE: {}".format(response['state']))
-        print("[+] POSTAL: {}".format(response['postal']))
-        print("[+] IPV4: {}".format(response['IPv4']))
+        print(f"{red}[+] found geolocation information  .{reset}")
+        print(f"{blue}[+] COUNTRY NAME:{reset}{yellow} {response['country_name']}{reset}")
+        print(f"{blue}[+] LATITUDE:{reset}{yellow} {response.get('latitude')}{reset}")
+        print(f"{blue}[+] LONGITUDE:{reset} {yellow}{response.get('longitude')}{reset}")
+        print(f"{blue}[+] CITY:{reset} {yellow}{response.get('city')}{reset}")
+        print(f"{blue}[+] COUNTRY CODE:{reset}{yellow} {response.get('country_code')}{reset}")
+        print(f"{blue}[+] STATE:{reset}{yellow} {response.get('state')}{reset}")
+        print(f"{blue}[+] POSTAL:{reset} {yellow}{response.get('postal')}{reset}")
+        print(f"{blue}[+] IPV4:{reset}{yellow} {response.get('IPv4')}{reset}")
     except Exception as e:
-        print("[-] Geolocation not found.")
+        print(f"{red}[-] Geolocation not found.{reset}")
         print("[-] Error:", e)
-
 
 
